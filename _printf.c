aef5d98182f87844520fc912c003e9f7f2e87b21
girliehappy <etetimh@gmail.com>
2023-05-30 11:41:18 -0700

    String, percentage and character format

--- _printf.c
+++ _printf.c
@@ -13,11 +13,11 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
+	/*if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+		return (-1);*/
 
 	for (; format[t] != '\0'; t++)
 	{
